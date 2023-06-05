19b84313606063849a1d80e9b5e4455e59ad4c6e
ClomosyDev <134625867+Clomosy@users.noreply.github.com>
2023-06-05 11:42:44 +0300

    Update code.pas

--- code.pas
+++ code.pas
@@ -8,11 +8,11 @@ var
     for I := 1 to 5 do
     begin
       case I of
-        1:ShowMessage('Sayı 1');
-        2:ShowMessage('Sayı 2');
+        1:ShowMessage('Number 1');
+        2:ShowMessage('Number 2');
         else
         begin
-          ShowMessage('Digerleri sayaç: '+IntToStr(I));
+          ShowMessage('Another Number: '+IntToStr(I));
         end;
       end;
     end;  
@@ -22,7 +22,7 @@ begin
   MyForm := TCLForm.Create(Self);
 
 
-  startBtn:= MyForm.AddNewButton(MyForm,'startBtn','GÖSTER');
+  startBtn:= MyForm.AddNewButton(MyForm,'startBtn','Show');
   startBtn.TextSettings.Font.Size:=50;
   startBtn.Align := alCenter;
   startBtn.Height := 50;
